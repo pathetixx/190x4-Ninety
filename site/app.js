d411c7f9ca6416f4153e7a38bb69c70164b42ebc
PathetiX <pow3r0807@gmail.com>
2026-07-13 03:54:22 +0000

    v0.2.14

--- site/app.js
+++ site/app.js
@@ -144,7 +144,7 @@ const translations = {
 };
 
 const releaseFallback = {
-  tagName: "v0.2.13",
+  tagName: "v0.2.14",
   htmlUrl: "https://github.com/pathetixx/190x4-Ninety/releases/latest",
   exeUrl: "https://github.com/pathetixx/190x4-Ninety/releases/latest",
   msiUrl: "https://github.com/pathetixx/190x4-Ninety/releases/latest",
